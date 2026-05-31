b5b04a84d69c6196b6741c86b10433d6ce7d8cc3
Abbas Darwich <fanamatic2.0@gmail.com>
2026-05-30 23:16:00 -0700

    Changed direction tuples and fixed test case bugs

--- test_files/test_solver.py
+++ test_files/test_solver.py
@@ -2,6 +2,8 @@ from solver import dfs
 import test_inputs
 
 def test_dfs():
+    open_edges = [[0b1111]*7 for _ in range(7)]
     test_inputs.test1_nodes[0][0] = -1
-    count = 0
-    assert dfs(test_inputs.test1_nodes, test_inputs.test1_edges, list(), 1, (0,0)) == test_inputs.test1_path
+    result = dfs(test_inputs.test1_nodes, open_edges, [], 1, (0, 0))
+    assert len(result) != 0
+    assert dfs(test_inputs.test1_nodes, test_inputs.test1_edges, [], 1, (0,0)) == test_inputs.test1_path
