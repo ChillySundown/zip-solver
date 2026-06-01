b216e9d9339da752cc7365adfafb0636333a1954
Abbas Darwich <fanamatic2.0@gmail.com>
2026-05-31 22:13:04 -0700

    We are passing test cases

--- test_files/test_solver.py
+++ test_files/test_solver.py
@@ -1,9 +1,15 @@
 from solver import dfs
 import test_inputs
+import copy
 
-def test_dfs():
+def test_dfs1():
     open_edges = [[0b1111]*7 for _ in range(7)]
-    test_inputs.test1_nodes[0][0] = -1
-    result = dfs(test_inputs.test1_nodes, open_edges, [], 1, (0, 0))
+    board1 = copy.deepcopy(test_inputs.test1_nodes)
+    board1[0][0] = -1
+    result = dfs(board1, open_edges, list(), 1, (0, 0))
     assert len(result) != 0
-    assert dfs(test_inputs.test1_nodes, test_inputs.test1_edges, [], 1, (0,0)) == test_inputs.test1_path
+
+def test_dfs2():
+    board2 = copy.deepcopy(test_inputs.test1_nodes)
+    board2[0][0] = -1
+    assert dfs(board2, test_inputs.test1_edges, list(), 1, (0,0))== test_inputs.test1_path
