0e551fd62ac7a2b6054431dea48354402b0abc5c
Abbas Darwich <fanamatic2.0@gmail.com>
2026-06-01 23:15:36 -0700

    Added hard test_case and renamed all edges to borders

--- test_files/test_solver.py
+++ test_files/test_solver.py
@@ -2,19 +2,24 @@ from solver import dfs
 import test_inputs
 import copy
 
-def test_no_edges():
-    open_edges = [[0b1111]*7 for _ in range(7)]
+def test_no_borders():
+    open_borders = [[0b1111]*7 for _ in range(7)]
     board = copy.deepcopy(test_inputs.test1_nodes)
     board[0][0] = -1
-    result = dfs(board, open_edges, list(), 1, (0, 0))
+    result = dfs(board, open_borders, list(), 1, (0, 0))
     assert len(result) != 0
 
 def test_dfs1():
     board1 = copy.deepcopy(test_inputs.test1_nodes)
     board1[0][0] = -1
-    assert dfs(board1, test_inputs.test1_edges, list(), 1, (0,0)) == test_inputs.test1_path
+    assert dfs(board1, test_inputs.test1_borders, list(), 1, (0,0)) == test_inputs.test1_path
 
 def test_dfs2():
     board2 = copy.deepcopy(test_inputs.test2_nodes)
     board2[2][5] = -1
-    assert dfs(board2, test_inputs.test2_edges, list(), 1, (2,5)) == test_inputs.test2_path
+    assert dfs(board2, test_inputs.test2_borders, list(), 1, (2,5)) == test_inputs.test2_path
+
+def test_dfs3():
+    board3 = copy.deepcopy(test_inputs.test3_nodes)
+    board3[7][3] = -1
+    assert dfs(board3, test_inputs.test3_borders, list(), 1, (7,3)) == test_inputs.test3_path
