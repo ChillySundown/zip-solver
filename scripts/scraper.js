94dc3384ad9d3e6ee915971e0af0dc4aef1852d8
Abbas Darwich <fanamatic2.0@gmail.com>
2026-06-07 23:45:48 -0700

    Creating place to store scripts

---
+++ scripts/scraper.js
@@ -0,0 +1,34 @@
+function make_empty_grid(size) {
+    return Array(size).fill().map(() => Array(size).fill(0))
+}
+
+const cells = document.querySelectorAll('[data-cell-idx]')
+let size = null
+switch(cells.length) {
+    case 36:
+        size = 6
+        break
+    case 49:
+        size = 7
+        break
+    case 64:
+        size = 8
+        break
+    default:
+        console.log("Error - Unforseen grid length error")
+        size = 0
+}
+console.log(size)
+const node_arr = make_empty_grid(size)
+const border_arr = make_empty_grid(size)
+console.log(node_arr)
+console.log(border_arr)
+
+cells.forEach(cell => {
+    let index = parseInt(cell.getAttribute('data-cell-idx'))
+    let r = Math.floor(index / size) 
+    let c = index % size
+    console.log(r)
+    console.log(c)
+
+})
