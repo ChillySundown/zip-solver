5581b1e0b2a5ac8810bbae1ec78e688904c8c7fe
Abbas Darwich <fanamatic2.0@gmail.com>
2026-06-15 23:32:08 -0700

    Sorry, forgot to save background:

--- scripts/background.js
+++ scripts/background.js
@@ -6,13 +6,13 @@ chrome.runtime.onMessage().addListener(async (path, sender) => {
 
     await chrome.debugger.attach({tabId: sender.tab.id}, 1.3);
 
-    for(let move of filledPath) {
+    for(let move of filledPath) { //Sends a keystroke command for each move
         chrome.debugger.sendCommand({tabId: sender.tab.id}, "Input.dispatchKeyEvent", {
             type: "KeyDown",
             key: utils.keyStrokes[move]
         });
 
-        await sleep(30);
+        await sleep(30); //Pauses so LinkedIn thinks I'm a human
     }
 
     await chrome.debugger.detach({tabId: sender.tab.id})
