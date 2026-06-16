f263007db7713a210235c611467904d021ed80e0
Abbas Darwich <fanamatic2.0@gmail.com>
2026-06-15 23:30:58 -0700

    Added service worker to simulate keystrokes

---
+++ scripts/background.js
@@ -0,0 +1,19 @@
+import * as utils from "../src/utils.js"
+
+//Pick up message from content to simulate keyStrokes
+chrome.runtime.onMessage().addListener(async (path, sender) => {
+    const {filledPath} = path;
+
+    await chrome.debugger.attach({tabId: sender.tab.id}, 1.3);
+
+    for(let move of filledPath) {
+        chrome.debugger.sendCommand({tabId: sender.tab.id}, "Input.dispatchKeyEvent", {
+            type: "KeyDown",
+            key: utils.keyStrokes[move]
+        });
+
+        await sleep(30);
+    }
+
+    await chrome.debugger.detach({tabId: sender.tab.id})
+}); 
